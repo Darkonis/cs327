63f20c2e66283fd316ab580cf22ba9b4d3f7e315
= <=>
2018-08-28 18:59:04 -0500

    Defined all functions that should be nessesary

---
+++ chessboard.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <chessboard.h>
+
+int beenVisited=0;
+
+void chessboardDefine(int x,int y, int isCircular)
+{
+
+}
+
+void makeAMove(int xPos,int yPos)
+{
+
+}
---
+++ chessboard.h
@@ -0,0 +1,5 @@
+//Developed by Darron Anderson
+#include <stdio.h>
+
+void chessboardDefine(int x,int y,int isCircular);
+void makeAMove(int xPos,int yPos);
